4b1ec197bdf0b2802ef5accdbf3eba5f3d89bff5
hanzala.arkhitech <hanzala.iqbal@arkhitech.com>
2023-09-25 10:35:35 +0500

    fixed messed up deleted file

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-
   get "/loginform",to: "cvs#loginform", as: :loginform
   get "/signupform",to: "cvs#signupform"
   post "/signupUser",to: "cvs#registerUser"
